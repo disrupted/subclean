cecad53283109e3eeed4ed30e4e6e9973039d437
disrupted <hi@salomonpopp.me>
2021-01-10 18:28:08 +0100

    Add ErrorProcessor
    fixes common errors

--- processors/processor.py
+++ processors/processor.py
@@ -1,4 +1,5 @@
 import re
+from typing import Callable, List
 
 from core.subtitle import Subtitle
 
@@ -6,20 +7,53 @@ from core.subtitle import Subtitle
 class Processor:
     def __init__(self, subtitle: Subtitle):
         self.subtitle = subtitle
+        self.operations: List[Callable] = []
 
-    def process(self):
-        pass
+    def process(self) -> Subtitle:
+        for i, section in enumerate(self.subtitle.sections):
+            for j, line in enumerate(section.lines):
+                for operation in self.operations:
+                    line = operation(line)
+                self.subtitle.sections[i].lines[j] = self.clean_dashes(line)
+        return self.subtitle
 
 
 class DialogProcessor(Processor):
     def __init__(self, subtitle: Subtitle):
         super().__init__(subtitle)
+        self.operations: List[Callable] = [self.clean_dashes]
 
     def clean_dashes(self, line: str) -> str:
         return re.sub(r"^(<\/?i>)*([-‐]+)(\s+)?", r"\1- ", line)
 
-    def process(self) -> Subtitle:
-        for i, section in enumerate(self.subtitle.sections):
-            for j, line in enumerate(section.lines):
-                self.subtitle.sections[i].lines[j] = self.clean_dashes(line)
-        return self.subtitle
+
+class ErrorProcessor(Processor):
+    def __init__(self, subtitle: Subtitle):
+        super().__init__(subtitle)
+        self.operations: List[Callable] = [
+            self.fix_hyphen,
+            self.fix_spaces,
+            self.trim_whitespace,
+            self.fix_ampersand,
+            self.fix_quote,
+            self.fix_music,
+        ]
+
+    def fix_spaces(self, line: str) -> str:
+        """Add missing spaces between sentences."""
+        return re.sub(r"\b([.?!]{1,2})([A-Z][a-z])", r"\1 \2", line)
+
+    def trim_whitespace(self, line: str) -> str:
+        return re.sub(r"\s{2,}", " ", line)
+
+    def fix_hyphen(self, line: str) -> str:
+        return re.sub(r"'’", "'", line)
+
+    def fix_ampersand(self, line: str) -> str:
+        return re.sub(r"&amp;", "&", line)
+
+    def fix_quote(self, line: str) -> str:
+        return re.sub(r"&quot;", '"', line)
+
+    def fix_music(self, line: str) -> str:
+        return re.sub(r"^#\s", "♪ ", line)
--- tests/test_dialogprocessor.py
+++ tests/test_dialogprocessor.py
@@ -1,10 +1,10 @@
 import pytest
 
 from core.subtitle import FakeSubtitle, Subtitle
-from processors.processor import DialogProcessor, Processor
+from processors.processor import DialogProcessor, ErrorProcessor, Processor
 
 
-class TestSubtitleParser:
+class TestDialogProcessor:
     @pytest.fixture()
     def processor(self):
         subtitle: Subtitle = FakeSubtitle()
@@ -17,3 +17,35 @@ class TestSubtitleParser:
         assert processor.clean_dashes("i-in") == "i-in"
         assert processor.clean_dashes("<i>-dialog.</i>") == "<i>- dialog.</i>"
         assert processor.clean_dashes("<i>-</i>dialog.") == "<i>- </i>dialog."
+
+
+class TestErrorProcessor:
+    @pytest.fixture()
+    def processor(self):
+        subtitle: Subtitle = FakeSubtitle()
+        processor: Processor = ErrorProcessor(subtitle)
+        return processor
+
+    def test_fix_spaces(self, processor: Processor):
+        assert (
+            processor.fix_spaces("First sentence.Second sentence.")
+            == "First sentence. Second sentence."
+        )
+
+    def test_trim_whitespace(self, processor: Processor):
+        assert (
+            processor.trim_whitespace("First sentence.  Second sentence.")
+            == "First sentence. Second sentence."
+        )
+
+    def test_fix_hyphen(self, processor: Processor):
+        assert processor.fix_hyphen("'’") == "'"
+
+    def test_fix_ampersand(self, processor: Processor):
+        assert processor.fix_ampersand("&amp;") == "&"
+
+    def test_fix_quote(self, processor: Processor):
+        assert processor.fix_quote("&quot;") == '"'
+
+    def test_fix_music(self, processor: Processor):
+        assert processor.fix_music("# lalalala") == "♪ lalalala"
