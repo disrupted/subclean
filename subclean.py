a3b9639c9319cb5b00ece78c404d34d2edb6ba59
disrupted <hi@salomonpopp.me>
2021-01-17 02:46:47 +0100

    Run default processors

--- subclean.py
+++ subclean.py
@@ -4,9 +4,11 @@ import logging
 
 from core.parser import SubtitleParser
 from core.subtitle import Subtitle
-from processors.processor import DialogProcessor, SDHProcessor
+from processors.processor import DialogProcessor, ErrorProcessor, SDHProcessor
 
 logging.basicConfig(format="%(levelname)s %(message)s", level=logging.DEBUG)
+
+DEFAULT_PROCESSORS = [SDHProcessor, DialogProcessor, ErrorProcessor]
 
 
 def main():
@@ -22,13 +24,9 @@ def main():
     parser = SubtitleParser()
     subtitle: Subtitle = parser.load(args.file.name)
     subtitle.parse()
-    # processor = DialogProcessor(subtitle)
-    # subtitle = processor.process()
-    processors = [SDHProcessor, DialogProcessor]
-    for processor in processors:
-        p = processor(subtitle)
-        subtitle = p.process()
-    # subtitle.print()
+    for processor in DEFAULT_PROCESSORS:
+        subtitle = processor(subtitle).process()
+    subtitle.print()
     subtitle.save()
 
 
