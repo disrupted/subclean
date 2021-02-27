3d0b1dceb238e5575d967e1b7628aa5276da6e8f
disrupted <hi@salomonpopp.me>
2021-02-27 23:30:09 +0100

    Add test

--- tests/test_linelengthprocessor.py
+++ tests/test_linelengthprocessor.py
@@ -49,6 +49,9 @@ class TestLineLengthProcessor:
         assert processor.split_dialog_chunks(
             [Line("hi"), Line("-bob"), Line("- bye"), Line("bob")]
         ) == [[Line("hi")], [Line("-bob")], [Line("- bye"), Line("bob")]]
+        assert processor.split_dialog_chunks(
+            [Line("-I'm gonna call the police,"), Line("this can't keep happening.")]
+        ) == [[Line("-I'm gonna call the police,"), Line("this can't keep happening.")]]
 
     def test_merge_short_lines(self, processor: LineLengthProcessor):
         sections = processor.subtitle.sections
@@ -62,3 +65,4 @@ class TestLineLengthProcessor:
         section = processor.process_section(sections[3])
         assert len(section.lines) == 1
         assert section.lines[0] == "- What if she's supposed to be with me?"
+        assert processor.process_section(sections[4]) == sections[4]
