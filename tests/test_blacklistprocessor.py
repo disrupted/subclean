12c312a3f3b9051202a81c8f446d5338d0853fb9
disrupted <hi@salomonpopp.me>
2021-11-23 20:23:35 +0100

    chore: add more test cases

--- tests/test_blacklistprocessor.py
+++ tests/test_blacklistprocessor.py
@@ -18,8 +18,15 @@ class TestBlacklistProcessor:
         return BlacklistProcessor(subtitle)
 
     def test_in_blacklist(self, fake_processor: BlacklistProcessor):
+        assert fake_processor.in_blacklist(Line("Advertise your product or brand here"))
+        assert fake_processor.in_blacklist(Line("contact www.OpenSubtitles.org today"))
+        assert fake_processor.in_blacklist(
+            Line('<font color="#ffff00">Provided by username</font>')
+        )
         assert fake_processor.in_blacklist(Line("[http://example.com]"))
         assert fake_processor.in_blacklist(Line("Visit https://another-example.com"))
+        assert fake_processor.in_blacklist(Line("find more under subs.link"))
+        assert fake_processor.in_blacklist(Line("twitter.com/username"))
 
     def test_process(self, sub_processor: BlacklistProcessor):
         assert len(sub_processor.subtitle.sections) == 2
