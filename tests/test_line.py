2638bb7cb837b837ea72df2bf3933be686b6c466
Salomon Popp <hi@salomonpopp.me>
2021-12-30 11:22:54 +0100

    feat: strip position styles (#101)

--- tests/test_line.py
+++ tests/test_line.py
@@ -4,9 +4,11 @@ from subclean.core.line import Line
 class TestLine:
     def test_strip_styles(self):
         assert Line("- <i>It's working.</i>").strip_styles() == "- It's working."
+        assert Line("{\an8}appears at top").strip_styles() == "appears at top"
 
     def test_len(self):
         assert len(Line("- <i>It's working.</i>")) == len("- It's working.")
+        assert len(Line("{\an8}appears at top")) == len("appears at top")
         assert len(
             Line('<i>Previously on <font color="#ffff00">"TV Show"</font>...</i>')
         ) == len('Previously on "TV Show"...')
