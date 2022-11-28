0aa5893d7adb6c5122337a27832d3284809b6b06
Salomon Popp <hi@salomonpopp.me>
2022-11-28 22:33:07 +0100

    chore: delete redundant init method (#210)

--- subclean/core/section/timing.py
+++ subclean/core/section/timing.py
@@ -5,8 +5,5 @@ class SectionTiming:
 
 
 class SrtSectionTiming(SectionTiming):
-    def __init__(self, start_time: str, end_time: str) -> None:
-        super().__init__(start_time, end_time)
-
     def __str__(self) -> str:
         return f"{self.start_time} --> {self.end_time}"
